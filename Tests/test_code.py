d04ba129bc507852b624128a8c64136c1fb393b5
Niko Darby <ndarby2050@gmail.com>
2020-01-22 15:27:50 -0500

    Updated test file

---
+++ Tests/test_code.py
@@ -0,0 +1,14 @@
+
+import imports_necessary
+import functions_necessary 
+import colorama 
+from colorama import Fore, Style
+import dt
+
+print(dt.Introduction)
+print(Fore.YELLOW + dt.Notice)
+print(Style.RESET_ALL)
+
+from functions_necessary import the_balance
+
+the_balance()
